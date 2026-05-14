f65af9188938698187b349f65c1554bbf52a6abc
ravitejaraju1999 <ravitejaraju1999@gmail.com>
2026-05-14 19:29:15 +0530

    route 53

--- r53.tf
+++ r53.tf
@@ -1,8 +1,9 @@
-resource "route53_record" "roboshop" {
-  count   = LENGTH(var.instances) # it returns count value dynamically based on lenth of list
+resource "aws_route53_record" "roboshop" {
+  count   = length(var.instances) # it returns count value dynamically based on length of list
   zone_id = var.zone_id
   name    = "${var.instances[count.index]}.${var.domain_name}" #mongodb.raviteja.store
   type    = "A"
   ttl     = 300
-  records = [aws_instance.terraform.*.public_ip]
+  records = [aws_instance.terraform[count.index].private_ip] # it returns private ip of each instance based on count index
+  allow_overwrite = true
 }
